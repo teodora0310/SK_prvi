967f57d8575eacb9d6b050521a315c1be8bc6672
teodora0310 <teodoraivic0310@gmail.com>
2024-11-12 22:59:54 +0100

    projekat

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+rootProject.name = "Sk_prvi_projekat"
+include("Pdf")
+include("CSV")
+include("Txt")
+include("Excel")
+include("App")
